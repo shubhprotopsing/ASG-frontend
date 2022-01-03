10489b4221b1c43e8371d90a3fa5533a608ad500
abhishekg495 <abhishekgupta23810306@gmail.com>
2022-01-03 22:22:01 +0530

    Added gradient to team member cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 function Card(props){
-    
     return(
-      <div className="h-80 w-80  border-2 border-gray-300 rounded-lg relative ">
+      <div className="h-80 w-80 border-2 border-gray-300 rounded-lg relative flex-col justify-start items-center mb-20">
 
-          <div className="card_header   h-24 bg-pink-600 rounded-t-md "></div>
-          <img src= {props.img} className="rounded-full h-20 w-24 absolute top-12 left-28" />
-          <div className="card_content flex flex-col justify-start items-center bg-white absolute top-40">
+          <div className="card_header h-36 bg-gradient-to-b from-pink-600 to-white rounded-t-md flex justify-center items-center">
+          <img src= {props.img} className="rounded-full h-20 w-24" />
+          </div>
+          <div className="card_content flex flex-col justify-start items-center bg-white">
                 <div className="card_name font-black text-lg ">{props.name}</div>
                 <div className="card_position font-medium text-base text-gray-500">{props.position}</div>
-                <div className="card_description text-xs">{props.description}</div>
+                <div className="card_description text-xs p-6">{props.description}</div>
           </div>
       </div>
     );
